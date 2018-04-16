982249f9d618e9b2cbe2fbfa690af9c1b4fa9395
MeGaDeTH91 <megadeth_91@abv.bg>
2018-04-16 16:17:02 +0300

    Part_XI_SignUpMenu_Added

---
+++ Forum.App/Commands/LogOutCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace Forum.App.Commands
+{
+    using Forum.App.Contracts;
+    using System;
+
+    public class LogOutCommand : ICommand
+    {
+        private ISession session;
+        private IMenuFactory menuFactory;
+
+        public LogOutCommand(ISession session, IMenuFactory menuFactory)
+        {
+            this.session = session;
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            this.session.Reset();
+
+            IMenu menu = this.menuFactory.CreateMenu("MainMenu");
+
+            return menu;
+        }
+    }
+}
--- Forum.App/Services/UserService.cs
+++ Forum.App/Services/UserService.cs
@@ -4,6 +4,7 @@
     using System;
     using Forum.DataModels;
     using Forum.Data;
+    using System.Linq;
 
     public class UserService : IUserService
     {
@@ -28,12 +29,46 @@
 
         public bool TryLogInUser(string username, string password)
         {
-            throw new NotImplementedException();
+            if(string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            User userToLog = forumData.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
+
+            session.Reset();
+            session.LogIn(userToLog);
+            
+            return true;
         }
 
         public bool TrySignUpUser(string username, string password)
         {
-            throw new NotImplementedException();
+            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length > 3;
+            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length > 3;
+
+            if(!validUsername || !validPassword)
+            {
+                throw new ArgumentException("Username and Password must be longer than 3 symbols!");
+            }
+
+            bool userAlreadyExists = forumData.Users.Any(x => x.Username == username);
+
+            if (userAlreadyExists)
+            {
+                throw new InvalidOperationException("Username taken!");
+            }
+
+            int newUserId = forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
+
+            User newUser = new User(newUserId, username, password);
+
+            forumData.Users.Add(newUser);
+            forumData.SaveChanges();
+
+            this.TryLogInUser(username, password);
+
+            return true;
         }
     }
 }
